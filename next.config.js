355db21d61cd7f302ebfe78dac7a2a1195f6ecdf
Vladyslavua <vladgershman@gmail.com>
2023-09-15 16:16:51 +0200

    init store for cms

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  images: {
+    domains: [
+      "tailwindui.com",
+      "res.cloudinary.com"
+    ]
+  }
 }
 
 module.exports = nextConfig
