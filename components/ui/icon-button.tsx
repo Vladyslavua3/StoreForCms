9cec400b09842bf7fcd4a59dc0fb6095cfb35e04
Vladyslavua <vladgershman@gmail.com>
2023-11-21 19:01:25 +0100

    Added reusable components

---
+++ components/ui/icon-button.tsx
@@ -0,0 +1,21 @@
+import {cn} from "@/lib/utils";
+import {FC, MouseEventHandler, ReactElement} from "react";
+
+interface IconButtonProps {
+    onClick?:MouseEventHandler<HTMLButtonElement> | undefined
+    icon:ReactElement
+    className?:string
+}
+
+const IconButton:FC<IconButtonProps> = ({onClick,icon,className}) => {
+  return(
+      <button
+          onClick={onClick}
+          className={cn('rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition',className)}
+      >
+          {icon}
+      </button>
+  )
+}
+
+export default IconButton
